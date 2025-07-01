08a9bc557908625a885f2ecba0244fee4240700c
syukrontanzilah <syukrontanzilah@gmail.com>
2025-07-01 07:18:14 +0700

    add project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+ <div className="h-screen p-4 ">
+  <Button variant={"destructive"} className="rounded-full">Click ME!</Button>
+
+ </div>
+  );
+}
